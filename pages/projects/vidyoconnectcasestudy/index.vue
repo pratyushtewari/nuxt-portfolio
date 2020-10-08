92e6b992d65cc58d6ff2bd11fd3789b388ffd4fd
Pratyush Tewari <tewari.pratyush@gmail.com>
2020-10-08 11:51:06 -0400

    Added project into for vidyoconnect

--- pages/projects/vidyoconnectcasestudy/index.vue
+++ pages/projects/vidyoconnectcasestudy/index.vue
@@ -23,7 +23,14 @@
               communication and solid interpersonal relationships. VidyoConnect
               is a meeting solution for team collaboration that speeds global
               alignment across your enterprise, allowing for better-informed
-              decisions, faster responses and deeper trust.
+              decisions, faster responses and deeper trust. Checkout the
+              <a
+                href="https://www.vidyo.com/video-conferencing-solutions/product/desktop-video-calling"
+                target="_blank"
+              >
+                product website
+              </a>
+              for latest information on this application.
             </p>
             <br />
           </c>
@@ -71,17 +78,104 @@
               {
                 title: 'HOW:',
                 definition:
-                  'Kickstarted UX at Vidyo, created a new mobile and desktop experience and developed a UI framework called MV2C to release the project'
+                  'Kickstarted UX design and research at Vidyo, created a new mobile and desktop experience and developed a UI framework called MV2C to release the services.'
               }
             ]"
           />
         </c>
       </grid>
-      <div class="max-700">
-        <div style="padding:56.25% 0 0 0;position:relative;">
+
+      <br />
+
+      <h2>Problem Statement</h2>
+
+      <p>
+        The legacy VidyoDesktop™ application was a very successful but a
+        monolithic application for video-conferencing. This was build in
+        <a href="https://www.qt.io/" target="_blank">Qt</a>
+        . As Vidyo’s strived to provide better UX and customizable white-labeled
+        products to clients, this framework was very time consuming if not
+        limiting.
+      </p>
+      <br />
+
+      <h2>My role</h2>
+      <h3>Senior UI/UX Designer and Engineer | Jan 2016 - May 2018</h3>
+      <p>
+        I led the front-end team for Vidyo Connect. Oversaw daily agile
+        processes and built the design language across all products and major
+        releases. One such upgrade to an existing deployment received 3.6
+        million calls minutes (140% increase) across 42,000 meetings (180%
+        increase) with 92,500 (280% increase) calls in first 6 days and has
+        grown to become the largest private video conferencing platform in the
+        world logging more than 3 Billion minutes of talk time and over 150
+        Million conference calls.
+      </p>
+      <h3>UI/UX Designer and Engineer | October 2014 - Dec 2015</h3>
+      <p>
+        I designed and developed the UX for Vidyo Connect. Defined the company’s
+        new UX strategy and collaborated with a team of designers and engineers
+        to upgrade existing products. Created a new web UI framework and
+        partnered with the SDK team, and enhanced the existing static video
+        platform to a highly customizable unified communication platform.
+      </p>
+
+      <br />
+      <h2>Journey</h2>
+      <h3>2014</h3>
+
+      <p class="center">
+        Below are the screenshots of VidyoDesktop™ application in 2014
+      </p>
+      <br />
+      <grid class col="2" col-s="1">
+        <c span="1">
+          <carousel
+            :navigation-enabled="true"
+            navigationClickTargetSize="16"
+            :per-page="1"
+            class="width400"
+          >
+            <slide><img src="./img/vidyodesktop/1.png" /></slide>
+            <slide><img src="./img/vidyodesktop/2.png" /></slide>
+          </carousel>
+        </c>
+        <c span="1">
+          <h4>The issues:</h4>
+
+          <ul>
+            <li>Hard to manage</li>
+            <li>
+              Versions after Versions (different clients has different features)
+            </li>
+            <li>Old SDK</li>
+            <li>Different designs on different platforms</li>
+            <li>Changes took a lot of time</li>
+            <li>Frustrations – on both Vidyo's and client's sides</li>
+          </ul>
+          <h4>The Ask:</h4>
+          <ul>
+            <li>Better UX</li>
+            <li>Faster to Design – more over a unified design</li>
+            <li>Faster to Develop</li>
+            <li>Faster to Change and adapt to new requirements</li>
+            <li>Faster to Customize</li>
+            <li>Faster to Incorporate into existing work-flow</li>
+            <li>Develop once Deploy everywhere on all platforms</li>
+          </ul>
+        </c>
+      </grid>
+      <div style="margin-top: 24px" class="max-700">
+        <div style="padding: 56.25% 0 0 0; position: relative">
           <iframe
             src="https://player.vimeo.com/video/240894889?color=ffffff&title=0&byline=0&portrait=0"
-            style="position:absolute;top:0;left:0;width:100%;height:100%;"
+            style="
+              position: absolute;
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 100%;
+            "
             frameborder="0"
             allow="autoplay;"
             allowfullscreen="false"
@@ -89,16 +183,6 @@
         </div>
         <script src="https://player.vimeo.com/api/player.js"></script>
         <br />
-        <p>
-          Please head over to
-          <a
-            href="https://www.vidyo.com/video-conferencing-solutions/product/desktop-video-calling"
-            target="_blank"
-          >
-            product website
-          </a>
-          for more information.
-        </p>
       </div>
     </div>
   </div>
@@ -115,8 +199,7 @@ export default {
       meta: [
         {
           name: 'description',
-          content:
-            'An Enterprise Meeting Solution for Team Collaboration'
+          content: 'An Enterprise Meeting Solution for Team Collaboration'
         },
         {
           property: 'og:title',
@@ -129,8 +212,7 @@ export default {
         },
         {
           property: 'og:description',
-          content:
-            'An Enterprise Meeting Solution for Team Collaboration'
+          content: 'An Enterprise Meeting Solution for Team Collaboration'
         },
         {
           hid: 'og:url',
@@ -144,8 +226,7 @@ export default {
         },
         {
           name: 'twitter:description',
-          content:
-            'An Enterprise Meeting Solution for Team Collaboration'
+          content: 'An Enterprise Meeting Solution for Team Collaboration'
         },
         {
           name: 'twitter:image',
